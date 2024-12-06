aa7baf00e285a59a0ce347facd24df9aba3cf99c
Yildirim Arifcan <Arifcan091@gmail.com>
2024-12-06 12:21:38 +0100

    Add project files.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EcosystemSimulatorProject.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EcosystemSimulatorProject.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
